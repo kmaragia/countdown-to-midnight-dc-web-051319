e2db4836536ebd81bc763a2f277c7efb9f761461
Kemunto Maragia <kmaragia@email.wm.edu>
2020-08-24 22:05:04 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,14 +4,17 @@ def countdown(int)
   count = 0
   while count < int 
    puts "#{int} SECOND(S)!"
+   count += 1
   end
   puts "HAPPY NEW YEAR !"
 end
 
 def countdown_with_sleep (int)
+  count = 0
   while count < int
     sleep(1)
     puts "#{int} SECOND(S)!"
+    count +=1
   end
   puts "HAPPY NEW YEAR !"
   
