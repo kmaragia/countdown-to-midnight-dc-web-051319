12487420cf7b07a6a8aa25c1f5b846ac86ff50ff
Kemunto Maragia <kmaragia@email.wm.edu>
2020-08-24 22:15:54 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,16 +2,16 @@
 
 def countdown(int)
   count = 1
-  while count < int 
+  while count <= int 
    puts "#{int} SECOND(S)!"
    int -= 1
   end
-  return "HAPPY NEW YEAR !"
+  return "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep (int)
   count = 1
-  while count < int
+  while count <= int
     sleep(1)
     puts "#{count} SECOND(S)!"
     count +=1
